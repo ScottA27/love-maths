010dd6b7ebc6972908523ce0f7fc5d27831d624a
ScottA27 <scotta27501@gmail.com>
2022-11-06 20:14:59 +0000

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// wait for the DOM to finish loading before running the game 
+// get the button elements and add event listeners to them 
+
+document.addEventListener('DOMContentLoaded')
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
